43871cf05fbf8a827b409b5f5df53733fe16c763
Shilpa <shilpa.as0208@gmail.com>
2020-09-09 08:56:26 -0400

    added react router dom for routing and brought in new component for adding recipes which would be an admin route

---
+++ frontend/src/components/Admin/AddRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const AddRecipe = () => {
+    console.log('we have a new component')
+
+    return (
+        <div>Test Admin</div>
+    )
+}
+
+export default AddRecipe
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,13 +2,21 @@ import React from 'react'
 import '../generated/tailwind.css'
 import Header from './Header'
 import Recipes from './Recipe/Recipes'
+import AddRecipe from './Admin/AddRecipe'
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header title='Recipe Hook'/>
-      <Recipes />
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={Recipes}/>
+          <Route path='/admin' compoennt={AddRecipe} />
+          <Redirect to='/' />
+        </Switch>
+      </HashRouter>
     </div>
   )
 }
